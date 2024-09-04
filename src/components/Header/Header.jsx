f6484e27a529df116820bebc5a9a2828057d1648
Amir.sh <mobilefactory012@gmail.com>
2024-09-04 23:44:01 +0330

    Add : GustOptionList & handleOptions function in Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,20 @@
 import { useState } from "react"
 import { MdLocationOn } from "react-icons/md"
-import { HiCalendar, HiSearch } from "react-icons/hi"
+import { HiCalendar, HiMinus, HiPlus, HiSearch } from "react-icons/hi"
 
 function Header() {
   const [destination, setDestination] = useState("")
+  const [openOptions, setOpenOptions] = useState(false)
+  const [options, setOptions] = useState({ adult: 1, children: 0, room: 1 })
+
+  const handleOptions = (name, operation) => {
+    setOptions((prev) => {
+      return {
+        ...prev,
+        [name]: operation === "inc" ? options[name] + 1 : options[name] - 1,
+      }
+    })
+  }
 
   return (
     <div className="header">
@@ -28,10 +39,14 @@ function Header() {
         </div>
 
         <div className="headerSearchItem">
-          <div id="optionDropDown">
-            {" "}
-            &bull;1 adult&bull;2 children &bull;3 room
+          <div id="optionDropDown" onClick={() => setOpenOptions(!openOptions)}>
+            &bull;{options.adult} adult &bull;
+            {options.children} Children &bull;
+            {options.room} room
           </div>
+          {openOptions && (
+            <GustOptionList handleOptions={handleOptions} options={options} />
+          )}
           <span className="seperator"></span>
         </div>
 
@@ -44,5 +59,54 @@ function Header() {
     </div>
   )
 }
-
 export default Header
+
+function GustOptionList({ options, handleOptions }) {
+  return (
+    <div className="guestOptions">
+      <OptionItem
+        handleOptions={handleOptions}
+        type="adult"
+        options={options}
+        miLimit={1}
+      />
+      <OptionItem
+        handleOptions={handleOptions}
+        type="children"
+        options={options}
+        miLimit={0}
+      />
+      <OptionItem
+        handleOptions={handleOptions}
+        type="room"
+        options={options}
+        miLimit={1}
+      />
+    </div>
+  )
+}
+
+function OptionItem({ options, type, miLimit, handleOptions }) {
+  return (
+    <div className="guestOptionItem">
+      <span className="optionText">{type}</span>
+      <div className="optionCounter">
+        <button
+          onClick={() => handleOptions(type, "dec")}
+          className="optionCounterBtn"
+          disabled={options[type] <= miLimit}
+        >
+          <HiMinus className="icon" />
+        </button>
+        <span className="optionCounterNumber">{options[type]}</span>
+
+        <button
+          className="optionCounterBtn"
+          onClick={() => handleOptions(type, "inc")}
+        >
+          <HiPlus className="icon" />
+        </button>
+      </div>
+    </div>
+  )
+}
