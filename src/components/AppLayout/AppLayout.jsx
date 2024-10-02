10c75041d588ff334d8c8acb5c409b425df1b93e
Amir.sh <mobilefactory012@gmail.com>
2024-10-02 16:30:16 +0330

    Edit : Hotel Component

--- src/components/AppLayout/AppLayout.jsx
+++ src/components/AppLayout/AppLayout.jsx
@@ -1,17 +1,32 @@
-import { Outlet } from "react-router-dom";
-import Map from "../Map/Map";
-import { useHotels } from "../context/HotelsProvider";
+// import { Outlet } from "react-router-dom";
+// import Map from "../Map/Map";
+// import { useHotels } from "../context/HotelsProvider";
+
+// function AppLayout() {
+//   const { hotels } = useHotels();
+
+//   return (
+//     <div className="appLayout">
+//       <div className="sidebar">
+//         <Outlet />
+//       </div>
+//       <Map markerLocations={hotels} />
+//     </div>
+//   );
+// }
+// export default AppLayout;
+
+import { Outlet } from "react-router-dom"
 
 function AppLayout() {
-  const { hotels } = useHotels();
-  
   return (
     <div className="appLayout">
       <div className="sidebar">
         <Outlet />
       </div>
-      <Map markerLocations={hotels} />
+      <div className="mapContainer">map</div>
     </div>
-  );
+  )
 }
-export default AppLayout;
+
+export default AppLayout
--- src/components/Hotels/Hotels.jsx
+++ src/components/Hotels/Hotels.jsx
@@ -1,37 +1,58 @@
-import { Link } from "react-router-dom";
-import Loader from "../Loader/Loader";
-import { useHotels } from "../context/HotelsProvider";
+// import { Link } from "react-router-dom";
+// import Loader from "../Loader/Loader";
+// import { useHotels } from "../context/HotelsProvider";
+
+// function Hotels() {
+//   const { isLoading, hotels, currentHotel } = useHotels();
+//   if (isLoading) return <Loader />;
+
+//   return (
+//     <div className="searchList">
+//       <h2>Search Results ({hotels.length})</h2>
+//       {hotels.map((item) => {
+//         return (
+//           <Link
+//             key={item.id}
+//             to={`/hotels/${item.id}?lat=${item.latitude}&lng=${item.longitude}`}
+//           >
+//             <div
+//               className={`searchItem ${
+//                 item.id === currentHotel?.id ? "current-hotel" : ""
+//               }`}
+//             >
+//               <img src={item.picture_url.url} alt={item.name} />
+//               <div className="searchItemDesc">
+//                 <p className="location">{item.smart_location}</p>
+//                 <p className="name">{item.name}</p>
+//                 €&nbsp;{item.price}&nbsp;
+//                 <span>night</span>
+//               </div>
+//             </div>
+//           </Link>
+//         );
+//       })}
+//     </div>
+//   );
+// }
+// export default Hotels;
+
+import { useSearchParams } from "react-router-dom"
+import useFetch from "../../hooks/useFetch"
+import Loader from "../Loader/Loader"
 
 function Hotels() {
-  const { isLoading, hotels, currentHotel } = useHotels();
-  if (isLoading) return <Loader />;
+  const { searchParams, setSearchParams } = useSearchParams()
+  const destination = searchParams.get("destination")
+  const room = JSON.parse(searchParams.get("options"))?.room
 
-  return (
-    <div className="searchList">
-      <h2>Search Results ({hotels.length})</h2>
-      {hotels.map((item) => {
-        return (
-          <Link
-            key={item.id}
-            to={`/hotels/${item.id}?lat=${item.latitude}&lng=${item.longitude}`}
-          >
-            <div
-              className={`searchItem ${
-                item.id === currentHotel?.id ? "current-hotel" : ""
-              }`}
-            >
-              <img src={item.picture_url.url} alt={item.name} />
-              <div className="searchItemDesc">
-                <p className="location">{item.smart_location}</p>
-                <p className="name">{item.name}</p>
-                €&nbsp;{item.price}&nbsp;
-                <span>night</span>
-              </div>
-            </div>
-          </Link>
-        );
-      })}
-    </div>
-  );
+  const { isLoading, data } = useFetch(
+    "http/localhost:5000/hotels",
+    `q = ${destination || ""} &accommodates_gte = ${room || ""}`
+  )
+
+  if (isLoading) <Loader />
+
+  return <div>{data.length}</div>
 }
-export default Hotels;
+
+export default Hotels
